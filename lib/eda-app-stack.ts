61284a13554bff74c3811d454e513b7b8d80520b
Evan Hearne <evanhearne02@gmail.com>
2024-11-22 17:54:35 +0000

    add imagetable dynamodb table

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -8,13 +8,28 @@ import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as sns from "aws-cdk-lib/aws-sns";
 import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 import { Construct } from "constructs";
+import { DynamoDB, DynamoDBClient } from "@aws-sdk/client-dynamodb";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EDAAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Create DynamoDB Table named ImageTable
+    const imageTable = new dynamodb.Table(
+      this,
+      "ImageTable",
+      {
+        partitionKey: {
+          name: "ImageName",
+          type: dynamodb.AttributeType.STRING
+        },
+        stream: dynamodb.StreamViewType.NEW_IMAGE
+      }
+    )
 
     const imagesBucket = new s3.Bucket(this, "images", {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
