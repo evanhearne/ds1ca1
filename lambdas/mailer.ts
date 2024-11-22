a61b8e17731bfed07c98a2c6838edbd4b23b732a
Evan Hearne <evanhearne02@gmail.com>
2024-11-22 17:19:04 +0000

    change mailer fn to receive msg from topic directly

--- lambdas/mailer.ts
+++ lambdas/mailer.ts
@@ -1,4 +1,4 @@
-import { SQSHandler } from "aws-lambda";
+import { SNSHandler } from "aws-lambda";
 import { SES_EMAIL_FROM, SES_EMAIL_TO, SES_REGION } from "../env";
 import {
   SESClient,
@@ -8,7 +8,7 @@ import {
 
 if (!SES_EMAIL_TO || !SES_EMAIL_FROM || !SES_REGION) {
   throw new Error(
-    "Please add the SES_EMAIL_TO, SES_EMAIL_FROM and SES_REGION environment variables in an env.js file located in the root directory"
+    "Please add the SES_EMAIL_TO, SES_EMAIL_FROM, and SES_REGION environment variables in an env.js file located in the root directory"
   );
 }
 
@@ -18,34 +18,38 @@ type ContactDetails = {
   message: string;
 };
 
-const client = new SESClient({ region: SES_REGION});
+const client = new SESClient({ region: SES_REGION });
 
-export const handler: SQSHandler = async (event: any) => {
-  console.log("Event ", JSON.stringify(event));
+export const handler: SNSHandler = async (event) => {
+  console.log("SNS Event: ", JSON.stringify(event));
+
   for (const record of event.Records) {
-    const recordBody = JSON.parse(record.body);
-    const snsMessage = JSON.parse(recordBody.Message);
+    try {
+      const snsMessage = JSON.parse(record.Sns.Message);
 
-    if (snsMessage.Records) {
-      console.log("Record body ", JSON.stringify(snsMessage));
-      for (const messageRecord of snsMessage.Records) {
-        const s3e = messageRecord.s3;
-        const srcBucket = s3e.bucket.name;
-        // Object key may have spaces or unicode non-ASCII characters.
-        const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
-        try {
+      if (snsMessage.Records) {
+        console.log("S3 Event Records: ", JSON.stringify(snsMessage.Records));
+
+        for (const messageRecord of snsMessage.Records) {
+          const s3Event = messageRecord.s3;
+          const srcBucket = s3Event.bucket.name;
+          const srcKey = decodeURIComponent(s3Event.object.key.replace(/\+/g, " "));
+
+          // Prepare email details
           const { name, email, message }: ContactDetails = {
             name: "The Photo Album",
             email: SES_EMAIL_FROM,
-            message: `We received your Image. Its URL is s3://${srcBucket}/${srcKey}`,
+            message: `We received your image. Its URL is s3://${srcBucket}/${srcKey}`,
           };
+
           const params = sendEmailParams({ name, email, message });
+
+          // Send email using SES
           await client.send(new SendEmailCommand(params));
-        } catch (error: unknown) {
-          console.log("ERROR is: ", error);
-          // return;
         }
       }
+    } catch (error) {
+      console.error("Error processing SNS message: ", error);
     }
   }
 };
@@ -61,14 +65,10 @@ function sendEmailParams({ name, email, message }: ContactDetails) {
           Charset: "UTF-8",
           Data: getHtmlContent({ name, email, message }),
         },
-        // Text: {.           // For demo purposes
-        //   Charset: "UTF-8",
-        //   Data: getTextContent({ name, email, message }),
-        // },
       },
       Subject: {
         Charset: "UTF-8",
-        Data: `New image Upload`,
+        Data: `New Image Upload`,
       },
     },
     Source: SES_EMAIL_FROM,
@@ -91,7 +91,7 @@ function getHtmlContent({ name, email, message }: ContactDetails) {
   `;
 }
 
- // For demo purposes - not used here.
+// For demo purposes - not used here.
 function getTextContent({ name, email, message }: ContactDetails) {
   return `
     Received an Email. 📬
